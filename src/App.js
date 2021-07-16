c8d50e3d6e1bdee7c6595645f28bb7194ac60527
Pankaj Janoti <pankajjanoti777@gmail.com>
2021-07-16 11:43:30 +0530

    Counter app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react"
+// import ReactDOM from 'react-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App=()=>{
+ 
+    let [score,setScore] = useState(0)   //score=variable  ---- setScore=function---- useState(0) gives default value
+
+    // onClick={setScore(score+1)} cannot be done because setScore will be called without clicking button , so we use onClick={arrow function}
+    return(
+        <div>
+            <h1 style={{textAlign:"center"}}>This is the score of Game</h1>
+            <h2 style={{textAlign:"center"}}>The present score is {score}</h2>
+            <button onClick={()=>score<25?setScore(score+1):""} className="button">Increment</button>
+            <button onClick={()=>score>0?setScore(score-1):""} className="button">Decrement</button>
+            <button onClick={()=>{setScore(0)}}className="button">Reset</button>
+ 
+        </div>
+    )
 }
 
-export default App;
+export default App
